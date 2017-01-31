70f903e9785ec3ef76be96e0783b3c8498901133
root <romainchateau@laposte.net>
2017-01-31 17:09:49 +0100

    Clean de librairies non utilisées

--- consumer/kafka_spark_mongodb.py
+++ consumer/kafka_spark_mongodb.py
@@ -44,6 +44,7 @@ my_spark = SparkSession \
     .config("spark.mongodb.output.uri", "mongodb://172.254.0.4:27017/test.coll") \
     .getOrCreate()
 
+
 people = my_spark.createDataFrame([("Bilbo Baggins",  50), ("Gandalf", 1000), ("Thorin", 195), ("Balin", 178), ("Kili", 77),
    ("Dwalin", 169), ("Oin", 167), ("Gloin", 158), ("Fili", 82), ("Bombur", None)], ["name", "age"])
 
