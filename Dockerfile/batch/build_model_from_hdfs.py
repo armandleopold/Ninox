8a2929aaded139c606c3afdf26aa79341ecb79a5
root <romainchateau@laposte.net>
2017-02-01 15:22:59 +0100

    Remove unused librairies in spark  & improve python build model
    - The model can't be build, we should use RDD of labeledPoint for data
    instead of dataframe  ....

--- Dockerfile/batch/build_model_from_hdfs.py
+++ Dockerfile/batch/build_model_from_hdfs.py
@@ -1,34 +1,37 @@
 """
  Get data from HDFS, build a model and save it to mongoDb.
-
- @Author : Romain CHATEAU
 """
 
 from __future__ import print_function
 
+# Libraries for predictive analytics
+from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.regression import LabeledPoint
 
+
+from pyspark.sql import SQLContext
+from pyspark.sql import Row
 from pyspark import SparkContext
 
+def parsePoint(line):
+    values = [x for x in line.split(',')]
+
+    return LabeledPoint(values[0], values[1:])
+
 sc = SparkContext(appName="BatchBuildModel")
-text_file = sc.textFile("hdfs://172.254.0.2:9000/user/root/initial.csv")
+data_file = sc.textFile("hdfs://172.254.0.2:9000/user/root/initial.csv")
+parts = data_file.map(lambda l: l.split(","))
+people = parts.map(lambda p: Row(name=p[0], age=p[1]))
+#people = parts.map(lambda p: Row(store=int(p[0]), date=p[1], dept=int(p[2]), weekly_sales=float(p[3]),is_holliday.x=p[4], temperature=float(p[5]), fuel_price=float(p[6]),markdown1=float(p[7]), markdown2=float(p[8]), markdown3=float(p[9]),markdown4=float(p[10]), markdown5=float(p[11]), cpi=float(p[12]),unemployment=float(p[13]), is_holliday.y=p[14], typed=p[15], size=int(p[16])))
 
-#parsedData = text_file.flatMap(lambda line: line.split(","))
+sqlContext = SQLContext(sc)
+df = sqlContext.createDataFrame(people)
+#parsedData = data_file.map(parsePoint)
 
-#output = parsedData.collect()
+model = RandomForest.trainClassifier(df, numClasses=2, categoricalFeaturesInfo={},
+                                     numTrees=3, featureSubsetStrategy="auto",
+                                     impurity='gini', maxDepth=4, maxBins=32)
 
-#for(text) in output:
-#	print(text)
-
-nonempty_lines = text_file.filter(lambda x: len(x) > 0)
-print ('Nonempty lines', nonempty_lines.count())
-
-words = nonempty_lines.flatMap(lambda x: x.split(' '))
-
-wordcounts = words.map(lambda x: (x, 1)) \
-                  .reduceByKey(lambda x, y: x+y) \
-                  .map(lambda x: (x[1], x[0])).sortByKey(False)
-
-print ('Top 100 words:')
-print (wordcounts.take(100))
 
 sc.stop()
